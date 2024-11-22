94e4152853ba1db3aab5f063f10a1782c55c4f82
OptikJaya <optikjayasribawono@gmail.com>
2024-11-22 09:03:20 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'optik-jaya-cache-v2.0.0';
-const VERSION = '2.0.0'; // Add version tracking
+const CACHE_NAME = 'optik-jaya-cache-v2.0.1';
+const VERSION = '2.0.1'; // Add version tracking
 const urlsToCache = [
     './',
     './index.html',
